1db3b66f3f6bd5a6e38d3b8ab505d9e0110df432
peterhu0929 <peterhu0929@gmail.com>
2017-07-12 02:07:21 +0800

    20170712

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import 'hammerjs';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
-import {MdCardModule, MdCheckboxModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
